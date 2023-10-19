0e007a14c6ef2a85a064e659c99f264d8f9c3887
Dmytro <dmytro.shvalikovskyi.ki.2020@lpnu.ua>
2023-10-19 21:29:37 +0300

    My commit

---
+++ ProjectCity-Yevhenii/src/main/java/ua/goit/Main.java
@@ -0,0 +1,14 @@
+package ua.goit;
+
+import ua.goit.FrontEnd.GameFrame;
+import ua.goit.FrontEnd.StartFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        StartFrame frame = new StartFrame();
+//        GameFrame gameFrame = new GameFrame();
+//        System.out.println(gameFrame.hashCode());
+        //test message
+
+    }
+}
